7002355979b2006a174c94c139ec121b738ea91a
ffraniel <francis.whitehead@googlemail.com>
2020-05-15 12:44:44 +0100

    all artist display structure

--- src/Pages/Artists.js
+++ src/Pages/Artists.js
@@ -6,6 +6,7 @@ import shuffle from "../Utility/shuffle";
 const Artists = () => {
   const [data, setData] = useState(null);
   const [currentPosition, setCurrentPosition] = useState(0);
+  const [isShowingAll, setIsShowingAll] = useState(true);
 
   useEffect(() => {
     let randomisedData = shuffle(musicData);
@@ -30,39 +31,108 @@ const Artists = () => {
     }
   };
 
+  //when you click one set ishowingall to false and set location on the one selected
+
   return (
-    <div className="text-center container mx-auto max-w-lg shadow-lg mb-6 font-rubik font-normal">
-      {data && (
-        <div className="mb-2 relative">
-          <div className="my-2 mx-4 z-10 relative">
-            <button
-              className="bg-gray-800 text-white mx-2 w-20 py-1 shadow-xl absolute left-0"
-              onClick={artistBack}
-            >
-              Back
-            </button>
-            <button
-              className="bg-gray-800 text-white mx-2 w-20 6 py-1 shadow-xl absolute right-0"
-              onClick={artistForward}
-            >
-              Forward
-            </button>
+    <div>
+      <div className="mx-auto max-w-4xl">
+        <button
+          disabled={!isShowingAll}
+          className={`${isShowingAll ? "bg-green-700" : "bg-green-400"}`}
+          onClick={() => {
+            setIsShowingAll(isShowingAll => !isShowingAll);
+          }}
+        >
+          Show One
+        </button>
+        <button
+          disabled={isShowingAll}
+          className={`${isShowingAll ? "bg-green-400" : "bg-green-700"}`}
+          onClick={() => {
+            setIsShowingAll(isShowingAll => !isShowingAll);
+          }}
+        >
+          Show All
+        </button>
+      </div>
+
+      {isShowingAll && !data && <h1>Loading</h1>}
+      {isShowingAll && musicData && (
+        <div className="text-center m-2 mx-auto max-w-4xl font-rubik font-normal border">
+          <h3>Artists</h3>
+          <div className="grid grid-cols-6">
+            {musicData.map((entry, arrayI) => (
+              <button
+                onClick={e => {
+                  e.preventDefault();
+                  // get place in data and set current position to that
+                  let country = entry.country;
+                  var arrayPosition;
+                  data.forEach((dataEntry, i) => {
+                    if (dataEntry.country === country) {
+                      arrayPosition = i;
+                    }
+                  });
+                  setCurrentPosition(arrayPosition);
+                  setIsShowingAll(false);
+                }}
+              >
+                <div
+                  className={`${
+                    arrayI === currentPosition ? "bg-green-200" : ""
+                  } hover:bg-red-300`}
+                >
+                  <div>
+                    <img src={entry.artistImg} alt={entry.artist} />
+                    <img src={entry.lilFlag} alt={entry.country} />
+                    <h4>{entry.country}</h4>
+                  </div>
+                  <div>
+                    <h3>{entry.artist}</h3>
+                    <h1>{entry.songName}</h1>
+                  </div>
+                </div>
+              </button>
+            ))}
           </div>
-          <ArtistFrontPage artist={data[currentPosition]} />
-          <div className="my-2 pb-4">
-            <button
-              className="bg-gray-800 text-white mx-2 w-20 py-1"
-              onClick={artistBack}
-            >
-              Back
-            </button>
-            <button
-              className="bg-gray-800 text-white mx-2 w-20 6 py-1"
-              onClick={artistForward}
-            >
-              Forward
-            </button>
-          </div>
+        </div>
+      )}
+
+      {!isShowingAll && (
+        <div className="text-center container mx-auto max-w-lg shadow-lg mb-6 font-rubik font-normal">
+          {data && (
+            <div className="mb-2 relative">
+              <div className="my-2 mx-4 z-10 relative">
+                <button
+                  className="bg-gray-800 text-white mx-2 w-20 py-1 shadow-xl absolute left-0"
+                  onClick={artistBack}
+                >
+                  Back
+                </button>
+                <button
+                  className="bg-gray-800 text-white mx-2 w-20 6 py-1 shadow-xl absolute right-0"
+                  onClick={artistForward}
+                >
+                  Forward
+                </button>
+              </div>
+              <ArtistFrontPage artist={data[currentPosition]} />
+              <div className="my-2 pb-4">
+                <button
+                  className="bg-gray-800 text-white mx-2 w-20 py-1"
+                  onClick={artistBack}
+                >
+                  Back
+                </button>
+                <button
+                  className="bg-gray-800 text-white mx-2 w-20 6 py-1"
+                  onClick={artistForward}
+                >
+                  Forward
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       )}
     </div>
